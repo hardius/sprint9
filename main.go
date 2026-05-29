a6c3e7451f10de752c435acb832bd152a20e8ceb
Dang Khyu Manh <d4ng777@yandex.ru>
2026-05-29 07:28:44 +0300

    first commit

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 const (
@@ -11,30 +14,95 @@ const (
 
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
-	// ваш код здесь
+	if size == 0 {
+		return []int{}
+	}
+
+	source := rand.NewSource(time.Now().UnixNano())
+	generator := rand.New(source)
+
+	if size == 1 {
+		return []int{generator.Int()}
+	}
+
+	array := make([]int, size)
+	for range array {
+		array = append(array, generator.Int())
+	}
+	return array
 }
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
-	// ваш код здесь
+	if len(data) == 0 {
+		return 0
+	}
+
+	max := data[0]
+	for i := range data {
+		if max < data[i] {
+			max = data[i]
+		}
+	}
+
+	return max
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
-	// ваш код здесь
+	chunkSize := len(data) / CHUNKS
+	if chunkSize < 2 {
+		return maximum(data)
+	}
+
+	arrMax := []int{}
+
+	var wg sync.WaitGroup
+	wg.Add(8)
+
+	for i := range CHUNKS {
+		start := i * chunkSize
+		end := start + chunkSize
+		go func(slice []int) {
+			arrMax = append(arrMax, maximum(slice))
+
+			wg.Done()
+		}(data[start:end])
+	}
+
+	remainder := len(data) % CHUNKS
+	if remainder != 0 {
+		start := chunkSize * CHUNKS
+		end := start + remainder
+		arrMax = append(arrMax, maximum(data[start:end]))
+	}
+
+	wg.Wait()
+	return maximum(arrMax)
 }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
-	// ваш код здесь
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
+	genArr := generateRandomElements(SIZE)
 
 	fmt.Println("Ищем максимальное значение в один поток")
-	// ваш код здесь
+	start := time.Now()
+	max := maximum(genArr)
+	elapsed := time.Since(start).Microseconds()
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
-	// ваш код здесь
+	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
+	start = time.Now()
+	max = maxChunks(genArr)
+	elapsed = time.Since(start).Microseconds()
+
+	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
+
+	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
+	start = time.Now()
+	max = maxChunks(genArr)
+	elapsed = time.Since(start).Microseconds()
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,31 @@
 package main
 
-// Пишите тесты в этом файле
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenerateFunc(t *testing.T) {
+	genArr := generateRandomElements(0)
+	require.Equal(t, []int{}, genArr)
+}
+func TestMaxDefaultCases(t *testing.T) {
+	genArr := generateRandomElements(100000)
+	require.Equal(t, maximum(genArr), maxChunks(genArr))
+
+	genArr = generateRandomElements(350607)
+	require.Equal(t, maximum(genArr), maxChunks(genArr))
+}
+
+func TestMaxZeroCase(t *testing.T) {
+	genArr := generateRandomElements(0)
+	require.Equal(t, 0, maximum(genArr))
+	require.Equal(t, maximum(genArr), maxChunks(genArr))
+}
+
+func TestMaxSingleCase(t *testing.T) {
+	genArr := generateRandomElements(1)
+	require.Equal(t, genArr[0], maximum(genArr))
+	require.Equal(t, maximum(genArr), maxChunks(genArr))
+}
